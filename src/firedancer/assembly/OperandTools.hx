5faaad64a65ca34c4eaaa00ae878792a4a4bf307
FAL <falworks.contact@gmail.com>
2020-08-29 22:56:59 +0900

    remove unused code

--- src/firedancer/assembly/OperandTools.hx
+++ src/firedancer/assembly/OperandTools.hx
@@ -17,27 +17,4 @@ class OperandTools {
 		};
 		return '${typeChar}var[\"$key\"]';
 	}
-
-	public static function tryGetOperandPair(a: Operand, b: Operand): Maybe<OperandPair> {
-		final pair: Null<OperandPair> = switch a {
-		case Null: null;
-		case Int(operandA):
-			switch b {
-			case Int(operandB): Int(operandA, operandB);
-			default: null;
-			}
-		case Float(operandA):
-			switch b {
-			case Float(operandB): Float(operandA, operandB);
-			default: null;
-			}
-		case Vec(operandA):
-			switch b {
-			case Vec(operandB): Vec(operandA, operandB);
-			default: null;
-			}
-		};
-
-		return Maybe.from(pair);
-	}
 }
