66c6560dfdea4902ef899e67bbc4014fa82975aa
FAL <falworks.contact@gmail.com>
2020-09-01 14:57:13 +0900

    demo: improve

--- demo/Global.hx
+++ demo/Global.hx
@@ -1,6 +1,6 @@
 class Global {
-	public static inline final width: UInt = 720;
-	public static inline final height: UInt = 960;
+	public static inline final width: UInt = 600;
+	public static inline final height: UInt = 800;
 
 	public static var world: Maybe<World> = Maybe.none();
 }
---
+++ demo/assets/bullet.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(100, 100);
+
+  const g = createGraphics(16, 16);
+  g.fill("#4d089a20");
+  g.stroke("#4d089a");
+  g.strokeWeight(1);
+  g.rect(1, 1, 14, 14);
+
+  image(g, 0, 0);
+  save(g, "bullet.png");
+}
