e69040902a3e4442bf390453643db99426cd0df5
FAL <falworks.contact@gmail.com>
2020-09-01 18:19:32 +0900

    script: remove unrolling methods

--- src/firedancer/script/nodes/Repeat.hx
+++ src/firedancer/script/nodes/Repeat.hx
@@ -9,32 +9,15 @@ import firedancer.script.expression.IntExpression;
 class Repeat extends AstNode implements ripper.Data {
 	final node: AstNode;
 	final repetitionCount: IntExpression;
-	var unrolling: Bool = false;
 
 	override inline function containsWait(): Bool {
 		return this.node.containsWait();
 	}
 
-	/**
-		Unrolls `this` loop if the loop count is a constant.
-		@return `this`
-	**/
-	public inline function unroll(): Repeat {
-		this.unrolling = true;
-		return this;
-	}
-
 	override function toAssembly(context: CompileContext): AssemblyCode {
 		final count = this.repetitionCount;
 		final body = this.node.toAssembly(context);
-
-		final constant = count.tryGetConstant();
-
-		final code = if (constant.isSome() && this.unrolling) {
-			loopUnrolled(0...constant.unwrap(), _ -> body);
-		} else {
-			loop(context, body, count);
-		}
+		final code = loop(context, body, count);
 
 		return code;
 	}
