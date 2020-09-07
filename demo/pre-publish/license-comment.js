b3a9a633b095a1deca9d487f0aa72dd591580986
FAL <falworks.contact@gmail.com>
2020-09-08 01:17:36 +0900

    demo: add license comments

---
+++ demo/pre-publish/license-comment.js
@@ -0,0 +1,15 @@
+/**
+ * Firedancer Demo
+ * https://github.com/fal-works/firedancer
+ *
+ * @copyright 2020 FAL Works
+ * @license MIT
+ */
+/**
+ * The following software may be included in this product:
+ *
+ * The Haxe Standard Library
+ * https://haxe.org
+ * © 2005-2016 Haxe Foundation,
+ * used under MIT License (https://haxe.org/foundation/open-source.html)
+ */
